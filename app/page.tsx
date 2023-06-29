1d8c5695001c25e025f16f9df050ac98a2c0b59f
mgkram18 <kram18.mg@gmail.com>
2023-06-28 17:10:24 -0700

    Your commit message

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <div>
       {/* Hero */}
-      <section className="bg-center bg-no-repeat bg-gray-800 rounded-xl">
-        <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+      <section className="bg-center bg-cover bg-no-repeat bg-gray-400 rounded-xl bg-[url('https://t4.ftcdn.net/jpg/02/12/78/05/360_F_212780575_jCx2m2E7XMr1xUmaMMnb3hbVMqApo2PO.jpg')] bg-blend-multiply border-gray-500 border-2 shadow-xl">
+        <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56 ">
           <h1 className="mb-4 py-2 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
             Specializing in Solar Panel Cleaning
           </h1>
@@ -44,10 +44,11 @@ export default function Home() {
           </div>
         </div>
       </section>
+
       {/* Pricing */}
       <section className="bg-white dark:bg-gray-900">
         <div className="py-8 mx-auto max-w-screen-xl lg:py-16">
-          <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 mb-8">
+          <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 mb-8 shadow-lg">
             <a
               href="#"
               className="bg-blue-100 text-blue-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-blue-400 mb-2"
@@ -92,7 +93,7 @@ export default function Home() {
             </a>
           </div>
           <div className="grid md:grid-cols-2 gap-8">
-            <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
+            <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 shadow-lg">
               <a
                 href="#"
                 className="bg-green-100 text-green-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-green-400 mb-2"
@@ -133,7 +134,7 @@ export default function Home() {
                   strokeWidth="2"
                   viewBox="0 0 24 24"
                   xmlns="http://www.w3.org/2000/svg"
-                  aria-hidden="true"
+                  // aria-hidden="true"
                 >
                   <path
                     strokeLinecap="round"
@@ -143,7 +144,7 @@ export default function Home() {
                 </svg>
               </a>
             </div>
-            <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
+            <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 shadow-lg">
               <a
                 href="#"
                 className="bg-purple-100 text-purple-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-purple-400 mb-2"
@@ -181,7 +182,7 @@ export default function Home() {
                   strokeWidth="2"
                   viewBox="0 0 24 24"
                   xmlns="http://www.w3.org/2000/svg"
-                  aria-hidden="true"
+                  // aria-hidden="true"
                 >
                   <path
                     strokeLinecap="round"
@@ -195,17 +196,128 @@ export default function Home() {
         </div>
       </section>
       {/* BREAK HERE */}
+      <section>
+        <div className="flex gap-4 justify-center mb-6 ">
+          {" "}
+          <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg sm:p-8 dark:bg-gray-800 dark:border-gray-700 shadow">
+            <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">
+              Single Story
+            </h5>
+            <div className="flex items-baseline text-gray-900 dark:text-white">
+              <span className="text-3xl font-semibold">$</span>
+              <span className="text-5xl font-extrabold tracking-tight">
+                250
+              </span>
+              <span className="ml-1 text-xl font-normal text-gray-500 dark:text-gray-400">
+                /10 panels
+              </span>
+            </div>
+            <div className="flex items-baseline text-gray-900 dark:text-white mt-2">
+              <span className="text-3xl font-semibold">$</span>
+              <span className="text-5xl font-extrabold tracking-tight">
+                350
+              </span>
+              <span className="ml-1 text-xl font-normal text-gray-500 dark:text-gray-400">
+                /15 panels
+              </span>
+            </div>
+            {/* List */}
+            <ul role="list" className="space-y-5 my-7">
+              <li className="flex space-x-3">
+                {/* Icon */}
+                <svg
+                  aria-hidden="true"
+                  className="flex-shrink-0 w-5 h-5 text-blue-600 dark:text-blue-500"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <title>Check icon</title>
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
+                  2 team members
+                </span>
+              </li>
+              {/* Add more list items here */}
+            </ul>
+            <button
+              type="button"
+              className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
+            >
+              Choose plan
+            </button>
+          </div>{" "}
+          <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 ">
+            <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">
+              Two Story
+            </h5>
+            <div className="flex items-baseline text-gray-900 dark:text-white">
+              <span className="text-3xl font-semibold">$</span>
+              <span className="text-5xl font-extrabold tracking-tight">
+                450
+              </span>
+              <span className="ml-1 text-xl font-normal text-gray-500 dark:text-gray-400">
+                /10 panels
+              </span>
+            </div>
+            <div className="flex items-baseline text-gray-900 dark:text-white mt-2">
+              <span className="text-3xl font-semibold">$</span>
+              <span className="text-5xl font-extrabold tracking-tight">
+                650
+              </span>
+              <span className="ml-1 text-xl font-normal text-gray-500 dark:text-gray-400">
+                /15 panels
+              </span>
+            </div>
+            {/* List */}
+            <ul role="list" className="space-y-5 my-7">
+              <li className="flex space-x-3">
+                {/* Icon */}
+                <svg
+                  aria-hidden="true"
+                  className="flex-shrink-0 w-5 h-5 text-blue-600 dark:text-blue-500"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <title>Check icon</title>
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
+                  2 team members
+                </span>
+              </li>
+              {/* Add more list items here */}
+            </ul>
+            <button
+              type="button"
+              className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
+            >
+              Choose plan
+            </button>
+          </div>
+        </div>
+      </section>
       {/* Connect Sections */}
       <section className="bg-white dark:bg-gray-900">
         <div className="py-8 mx-auto max-w-screen-xl lg:py-8">
-          <div className="bg-gray-800 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 mb-8">
-            <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl shadow">
+          <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 shadow-lg dark:border-gray-700 rounded-lg p-8 md:p-12 mb-8">
+            <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl">
               Lets Connect!
             </h1>
             <div className="">
               <label
                 htmlFor="message"
-                className="mb-4 text-xl tracking-tight leading-none text-white shadow"
+                className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4"
               >
                 Email
               </label>
@@ -232,7 +344,7 @@ export default function Home() {
               <div className="py-8">
                 <label
                   htmlFor="message"
-                  className="mb-4 text-xl tracking-tight leading-none text-white shadow"
+                  className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4 "
                 >
                   Your message
                 </label>
@@ -268,6 +380,48 @@ export default function Home() {
           </div>
         </div>
       </section>
+      <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+        <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+          <div className="sm:flex sm:items-center sm:justify-between">
+            <a
+              href="https://flowbite.com/"
+              className="flex items-center mb-4 sm:mb-0"
+            >
+              <Image src="/logo.png" alt="logo" width={250} height={150} />
+            </a>
+            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+              <li>
+                <a href="#" className="mr-4 hover:underline md:mr-6 ">
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="#" className="mr-4 hover:underline md:mr-6">
+                  Privacy Policy
+                </a>
+              </li>
+              <li>
+                <a href="#" className="mr-4 hover:underline md:mr-6 ">
+                  Licensing
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+          <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+          <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © 2023{" "}
+            <a href="https://flowbite.com/" className="hover:underline">
+              Socal Solar Wash™
+            </a>
+            . All Rights Reserved.
+          </span>
+        </div>
+      </footer>
     </div>
   );
 }
