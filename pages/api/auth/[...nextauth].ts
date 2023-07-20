c2b703ee8fb64f4cc63769c58b5a8acd15c87412
mgkram18 <kram18.mg@gmail.com>
2023-07-20 10:22:54 -0700

    test no next auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,19 +1,20 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
+// import NextAuth from "next-auth";
+// import GoogleProvider from "next-auth/providers/github";
 
-const prisma = new PrismaClient();
+// import { PrismaAdapter } from "@next-auth/prisma-adapter";
+// import { PrismaClient } from "@prisma/client";
 
-export const options = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-    // ...add more providers here
-  ],
-};
+// const prisma = new PrismaClient();
 
-export default NextAuth(options);
+// export const options = {
+//   adapter: PrismaAdapter(prisma),
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID as string,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+//     }),
+//     // ...add more providers here
+//   ],
+// };
+
+// export default NextAuth(options);
